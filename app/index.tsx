16e9d94685b89822eac00e746c48f3d51d3a207a
Vikash Vishwakarma <vishwakarmavikash673@gmail.com>
2026-05-22 18:19:41 +0530

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-lg font-bold text-blue-100">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
